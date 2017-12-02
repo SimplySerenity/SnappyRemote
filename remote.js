01a7a9b7800146ea5e0b6b46f64571fecffa1330
SimplySerenity <mcgoughboy@gmail.com>
2017-12-01 16:04:03 -0800

    the basics are done

---
+++ remote.js
@@ -0,0 +1,40 @@
+function getRemoteAddress(){
+	return "http://" + document.getElementById("remoteAddress").value;
+}
+
+function upButton(){
+	httpRequest = new XMLHttpRequest();
+	var requestURI = getRemoteAddress() + "/?up=1";
+	httpRequest.open("get", requestURI, true);
+	httpRequest.send();
+}
+
+function downButton(){
+	httpRequest = new XMLHttpRequest();
+	var requestURI = getRemoteAddress() + "/?down=1";
+	httpRequest.open("get", requestURI, true);
+	httpRequest.send();
+}
+
+function enterButton(){
+	httpRequest = new XMLHttpRequest();
+	var requestURI = getRemoteAddress() + "/?select=true";
+	httpRequest.open("get", requestURI, true);
+	httpRequest.send();
+}
+
+function sendText(){
+	httpRequest = new XMLHttpRequest();
+	var requestURI = getRemoteAddress() + "/?text=" + document.getElementById("keyboardInput").value;
+	httpRequest.open("get", requestURI, true);
+	httpRequest.send();
+	closeKeyboard();
+}
+
+function openKeyboard(){
+	document.getElementById("keyboardModal").classList.add("is-active");
+}
+
+function closeKeyboard(){
+	document.getElementById("keyboardModal").classList.remove("is-active");
+}
